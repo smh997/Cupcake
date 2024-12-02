4338f4cb877df4efbcf003a41777132b777f7d38
smh10 <smh10.mh@gmail.com>
2024-12-02 16:53:41 -0500

    Sending order summary to other apps using intent is available now.

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,8 @@
  */
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -140,10 +142,11 @@ fun CupcakeApp(
                 )
             }
             composable(CupcakeScreen.Summary.name){
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
                     onSendButtonClicked = { subject: String, summary: String ->
-
+                        shareOrder(context, subject, summary)
                     },
                     onCancelButtonClicked = {
                         cancelOrderAndNavigateToStart(viewModel, navController)
@@ -153,6 +156,24 @@ fun CupcakeApp(
             }
         }
     }
+}
+
+private fun shareOrder(
+    context: Context,
+    subject:String,
+    summary: String
+){
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
 }
 
 private fun cancelOrderAndNavigateToStart(
