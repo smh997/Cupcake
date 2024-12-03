dc9a66418eb94457f4325517fc12a50744b9108d
smh10 <smh10.mh@gmail.com>
2024-12-03 18:51:36 -0500

    Nav Host setup is done.

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -1,4 +1,25 @@
 package com.example.cupcake.test
 
-class CupcakeScreenNavigationTest {
+import com.example.cupcake.CupcakeApp
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Before
+import org.junit.Rule
+
+@get:Rule
+val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+private lateinit var navController: TestNavHostController
+
+@Before
+fun setupCupcakeNavHost(){
+    composeTestRule.setContent {
+        navController = TestNavHostController(LocalContext.current).apply {
+            navigatorProvider.addNavigator(ComposeNavigator())
+        }
+        CupcakeApp(navController = navController)
+    }
 }
